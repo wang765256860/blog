2e31864b976bc376b37c0306456a46c55fa429d9
wangzhen <wang765256860@gmail.com>
2018-10-07 19:24:37 +0800

    set env:

--- wsgi.py
+++ wsgi.py
@@ -5,6 +5,9 @@ from app import create_app, db
 from app.models import User, Role, Permission, Post, Follow, Comment
 
 
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
 
